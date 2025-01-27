e6460d0e2159ec7acf7316d7097695d9e37553f8
frandd07 <150445758+frandd07@users.noreply.github.com>
2025-01-27 16:45:12 +0100

    Arreglo

--- src/app/articulo/create/page.js
+++ src/app/articulo/create/page.js
@@ -1,80 +1,95 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-export default function CrearArticulo(){
-    const [titulo,setTitulo] = useState("")
-    const [contenido,setContenido] = useState("")
-    const [autor,setAutor] = useState("")
+export default function CrearArticulo() {
+    const [titulo, setTitulo] = useState("");
+    const [contenido, setContenido] = useState("");
+    const [autor, setAutor] = useState("");
+    const [fecha_publicacion, setFecha_publicacion] = useState("");
 
-    async function crearArticulo(e){
+    useEffect(() => {
+        setFecha_publicacion(new Date().toISOString().slice(0, 16)); 
+    }, []);
+
+    async function crearArticulo(e) {
         e.preventDefault();
 
-        if(titulo !== "" && contenido !== "" && autor !== ""){
+        if (titulo !== "" && contenido !== "" && autor !== "") {
             const response = await fetch("/api/articulo", {
-                method:'POST',
-                headers: {"Content-Type": "application/json"},
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({
                     articulo: {
                         titulo: titulo,
                         contenido: contenido,
                         autor: autor,
+                        fecha_publicacion: fecha_publicacion
                     }
                 })
-            })
-    
+            });
+
             if (response.ok) {
                 alert("Artículo creado exitosamente.");
                 setTitulo("");
                 setContenido("");
                 setAutor("");
-              } else {
+                setFecha_publicacion(new Date().toISOString().slice(0, 16));
+            } else {
                 alert("Hubo un error al crear el artículo");
-              }
-        }else{
-            alert("Error al crear el artículo")
+            }
+        } else {
+            alert("Error al crear el artículo");
         }
-
-       
     }
 
-    return(
+    return (
         <div>
-            <h1>Añadir articulo</h1>
+            <h1>Añadir artículo</h1>
             <form onSubmit={crearArticulo}>
                 <label>
-                    Titulo:
-                    <input 
-                    type="text"
-                    value={titulo}
-                    onChange={(e) => setTitulo(e.target.value)}
-                    maxLength={150}
-                    required
+                    Título:
+                    <input
+                        type="text"
+                        value={titulo}
+                        onChange={(e) => setTitulo(e.target.value)}
+                        maxLength={150}
+                        required
                     />
                 </label>
-                <br/>
+                <br />
                 <label>
                     Contenido:
-                    <input 
-                    type="text"
-                    value={contenido}
-                    onChange={(e) => setContenido(e.target.value)}
-                    required
+                    <input
+                        type="text"
+                        value={contenido}
+                        onChange={(e) => setContenido(e.target.value)}
+                        required
                     />
                 </label>
-                <br/>
+                <br />
                 <label>
                     Autor:
-                    <input 
-                    type="text"
-                    value={autor}
-                    onChange={(e) => setAutor(e.target.value)}
-                    required
+                    <input
+                        type="text"
+                        value={autor}
+                        onChange={(e) => setAutor(e.target.value)}
+                        required
                     />
                 </label>
-                <br/>
-                <input type="submit" value="Crear artículo"/>
+                <br />
+                <label>
+                    Fecha de publicación:
+                    <input
+                        type="datetime-local"
+                        value={fecha_publicacion}
+                        onChange={(e) => setFecha_publicacion(e.target.value)}
+                        required
+                    />
+                </label>
+                <br />
+                <input type="submit" value="Crear artículo" />
             </form>
         </div>
-    )
+    );
 }
